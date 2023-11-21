999207583ff1ad91723027e323e1a6d35ea2b02f
tasnimmh200 <you@example.com>
2023-11-21 13:00:56 +0200

    tasnim_mhbob_alhara

--- Linked_List_Homework/LinkedList.cs
+++ Linked_List_Homework/LinkedList.cs
@@ -24,20 +24,60 @@ namespace Linked_List_Homework
             // consider when the list is empty
 
             // create a new node with the given data
+            Node temp = new Node(val);
+            if (First == null) 
+            { First = temp; 
+                return; 
+            }
+            Node move = First;
+            while (move.Next != null) 
+            { move = move.Next; }
+            move.Next = temp;
+
         }
         public void RemoveKey(int key)
         {
             // TODO: search for the key and remove it from the list
             // consider when the key does not exist and when the list is empty
+            if (First==null)
+            {
+                return;
+            }
+            Node move = First;
+            if (First.Data==key)
+            {
+                First = First.Next;
+                move.Next = null;return;
+            }
+            while (move.Next != null && move.Next.Data != key)
+            {
+                move = move.Next;
+            }
+           move.Next= move.Next.Next;
         }
         public void Merge(LinkedList other_list)
         {
             // TODO: merge this list with the other list
+            Node move = First;
+            while (move.Next!=null)
+            {
+                move = move.Next;
+            }
+            move.Next = other_list.First;
         }
         public void Reverse()
         {
             // TODO: revers the nodes of this list
             // initialize three pointers: prev, curr, and next
+            Node prev = null, curr =First, next = null;
+            while (curr!=null)
+            {
+                next = curr.Next;
+                curr.Next = prev;
+                prev = curr;
+                curr = next;
+            }
+            First = prev;
         }
     }
 }
